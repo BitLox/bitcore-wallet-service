a25d17956ac995b3b16bfa3e5193df0f469f4d65
bojack <g@g.gg>
2017-07-08 11:03:39 +0800

    add some log messages

--- config.js
+++ config.js
@@ -40,7 +40,7 @@ var config = {
   blockchainExplorerOpts: {
     livenet: {
       provider: 'insight',
-      url: 'https://insight.bitpay.com:443',
+      url: 'http://localhost:3001',
     },
     testnet: {
       provider: 'insight',
